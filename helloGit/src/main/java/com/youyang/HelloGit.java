def2b374a479503d746b340bd8ff957f559bd1c4
yyangjian <1475136846@qq.com>
2021-04-12 21:41:38 +0800

    添加文件

---
+++ helloGit/src/main/java/com/youyang/HelloGit.java
@@ -0,0 +1,13 @@
+package com.youyang;
+
+/**
+ * @Author: yangjian
+ * @Data: 2021/04/12 - 21:21
+ * @Description:
+ * @Version: 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
